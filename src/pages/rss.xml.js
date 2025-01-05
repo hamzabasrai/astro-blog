8655c46fb47a71e35fc0ec0c535b18327b2d0299
Hamza Basrai <hamza.basrai@gmail.com>
2025-01-05 15:00:49 -0500

    Update RSS feed to use blog collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+	const posts = await getCollection('blog');
 	return rss({
 		title: "Hamza's Astro Blog",
 		description: 'A blog about learning Astro',
 		site: context.site,
-		items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+		items: posts.map(post => ({
+			title: post.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			link: `/posts/${post.id}`,
+		})),
 		customData: `<language>en-us</language>`,
 	});
 }
